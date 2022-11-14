4b23ae1ef4f0265de10dbdb49babad40c3e56af8
Andrei Toma <atoma@theworkshop.com>
2022-11-14 16:30:38 +0000

    Updated bash file

--- angular-app/initialise.sh
+++ angular-app/initialise.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 echo "Installing node and angular"
-apt -y update && apt -y upgrade && apt install -y curl && apt install -y npm 
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-apt install nodejs -y
-npm install -g @angular/cli 
-cd assets
-npm install
-clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
+curl -sL https://deb.nodesource.com/setup_16.x | bash - && apt install nodejs -y && npm install -g @angular/cli && cd assets && npm install && clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
 npm run start
