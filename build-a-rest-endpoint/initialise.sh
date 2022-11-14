262949e4f453117290275d30138d109cced1c009
Andrei Toma <atoma@theworkshop.com>
2022-11-14 14:54:37 +0000

    Bipass upgrade

--- build-a-rest-endpoint/initialise.sh
+++ build-a-rest-endpoint/initialise.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing node and angular"
-apt update && apt upgrade && apt install -y curl && apt install npm 
+apt update && apt -y upgrade && apt install -y curl && apt install npm 
 curl -s https://deb.nodesource.com/setup_16.x | sudo bash
 apt install nodejs -y
 npm install -g @angular/cli && clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
