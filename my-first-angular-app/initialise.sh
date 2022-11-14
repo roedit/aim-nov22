0476a958582a6d29b75f673c39b70f64d522781b
Andrei Toma <atoma@theworkshop.com>
2022-11-14 16:02:00 +0000

    Renamed folder to assets

--- my-first-angular-app/initialise.sh
+++ my-first-angular-app/initialise.sh
@@ -5,7 +5,7 @@ apt update -y && apt upgrade -y && apt install -y curl && apt -y install npm
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt install nodejs -y
 npm install -g @angular/cli 
-cd cats
+cd assets
 npm install
 clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
 npm run start
