4da2ab1aba721e7420d3a5f41d5378646c433c0e
Andrei Toma <atoma@theworkshop.com>
2022-11-14 15:44:42 +0000

    Setup ui folder structure

--- build-a-rest-endpoint/initialise.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing node and angular"
-apt update && apt -y upgrade && apt install -y curl && apt install npm 
-curl -s https://deb.nodesource.com/setup_16.x | sudo bash
-apt install nodejs -y
-npm install -g @angular/cli && clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
---
+++ my-first-angular-app/initialise.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing node and angular"
+apt update -y && apt upgrade -y && apt install -y curl && apt -y install npm 
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+apt install nodejs -y
+npm install -g @angular/cli 
+cd cats
+npm install
+npm run start
+clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
