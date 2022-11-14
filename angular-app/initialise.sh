98088ac04ac17174de88a3f5e97b7ac63ee08c84
Andrei Toma <atoma@theworkshop.com>
2022-11-14 16:25:19 +0000

    Setup initialise

--- angular-app/initialise.sh
+++ angular-app/initialise.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
-echo "Installing OpenJDK 11 and Maven"
-apt --yes install maven && cd assets && clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
+echo "Installing node and angular"
+apt -y update && apt -y upgrade && apt install -y curl && apt install -y npm 
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+apt install nodejs -y
+npm install -g @angular/cli 
+cd assets
+npm install
+clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
+npm run start
