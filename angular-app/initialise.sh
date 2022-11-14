faf1430a7f919252394780712c62cdaf6e8416af
Andrei Toma <atoma@theworkshop.com>
2022-11-14 17:52:36 +0000

    Install netstat

--- angular-app/initialise.sh
+++ angular-app/initialise.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Installing node and angular"
-curl -sL https://deb.nodesource.com/setup_16.x | bash - && apt install nodejs -y && npm install -g @angular/cli && cd assets && npm install && clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
+apt install net-tools && curl -sL https://deb.nodesource.com/setup_16.x | bash - && apt install nodejs -y && npm install -g @angular/cli && cd assets && npm install && clear && echo "Welcome to the tutorial, all the dependencies are installed, and you should be good to go!"
 npm run start
